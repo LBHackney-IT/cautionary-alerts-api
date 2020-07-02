e8073294b802f2ebe5ef126ac12fc8b58ac60cd0
elcorbs <emma@madetech.com>
2020-07-02 12:17:21 +0100

    Remove entity to domain mappings

--- CautionaryAlertsApi/V1/Factories/EntityFactory.cs
+++ CautionaryAlertsApi/V1/Factories/EntityFactory.cs
@@ -1,19 +1,14 @@
-using CautionaryAlertsApi.V1.Domain;
 using CautionaryAlertsApi.V1.Infrastructure;
 
 namespace CautionaryAlertsApi.V1.Factories
 {
     public static class EntityFactory
     {
-        public static CautionaryAlert ToDomain(this PersonAlert personAlert)
+        public static void ToDomain(this PersonAlert personAlert)
         {
             //TODO: Map the rest of the fields in the domain object.
             // More information on this can be found here https://github.com/LBHackney-IT/lbh-base-api/wiki/Factory-object-mappings
 
-            return new CautionaryAlert
-            {
-                Id = personAlert.Id,
-            };
         }
     }
 }
