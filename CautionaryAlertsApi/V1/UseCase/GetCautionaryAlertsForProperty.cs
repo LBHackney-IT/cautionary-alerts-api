792e766d9a8d352e4b00a80d2d90b7d6e00f3c10
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-12-08 09:27:42 +0000

    return an empty array for properties

--- CautionaryAlertsApi/V1/UseCase/GetCautionaryAlertsForProperty.cs
+++ CautionaryAlertsApi/V1/UseCase/GetCautionaryAlertsForProperty.cs
@@ -21,7 +21,7 @@ namespace CautionaryAlertsApi.V1.UseCase
         {
             var response = _gateway.GetCautionaryAlertsForAProperty(propertyReference);
 
-            if (response == null || (!response.Alerts?.Any() ?? true))
+            if (response == null)
             {
                 throw new PropertyAlertNotFoundException();
             }
