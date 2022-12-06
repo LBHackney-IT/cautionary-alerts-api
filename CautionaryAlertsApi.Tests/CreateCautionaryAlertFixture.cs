7b659c735839034eddac4b26179a80866d69a8fa
humulla <43747248+humulla@users.noreply.github.com>
2022-12-06 11:22:56 +0000

    AssetDetails optional

--- CautionaryAlertsApi.Tests/CreateCautionaryAlertFixture.cs
+++ CautionaryAlertsApi.Tests/CreateCautionaryAlertFixture.cs
@@ -35,5 +35,29 @@ namespace CautionaryAlertsApi.Tests
 
             return cautionaryAlert;
         }
+
+        public static CreateCautionaryAlert GenerateValidCreateCautionaryAlertWithoutAssetDetailsFixture(string defaultString, Fixture fixture)
+        {
+            var alert = fixture.Build<Alert>()
+                .With(x => x.Code, defaultString[..CreateCautionaryAlertConstants.ALERTCODELENGTH])
+                .With(x => x.Description, defaultString[..CreateCautionaryAlertConstants.ALERTDESCRIPTION])
+                .Create();
+
+
+            var personDetails = fixture.Build<PersonDetails>()
+                .With(x => x.Name, defaultString[..CreateCautionaryAlertConstants.PERSONNAMELENGTH])
+                .Create();
+
+            var cautionaryAlert = fixture.Build<CreateCautionaryAlert>()
+                .With(x => x.Alert, alert)
+                .With(x => x.PersonDetails, personDetails)
+                .With(x => x.IncidentDescription, defaultString[..CreateCautionaryAlertConstants.INCIDENTDESCRIPTIONLENGTH])
+                .With(x => x.IncidentDate, fixture.Create<DateTime>().AddDays(-1))
+                .With(x => x.AssureReference, defaultString[..CreateCautionaryAlertConstants.ASSUREREFERENCELENGTH])
+                .Without(x => x.AssetDetails)
+                .Create();
+
+            return cautionaryAlert;
+        }
     }
 }
