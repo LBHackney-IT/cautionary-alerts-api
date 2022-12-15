fc1c6c133a2a37594534954c91b4c2ad1b357c91
humulla <43747248+humulla@users.noreply.github.com>
2022-12-15 15:17:36 +0000

    updated policy

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -92,7 +92,7 @@ data "aws_iam_policy_document" "sns-topic-policy" {
 
       principals {
         type        = "AWS"
-        identifiers = ["arn:aws:iam::${data.account_id}:role/LBH_Circle_CI_Deployment_Role"]
+        identifiers = "*"
       }
 
       resources = [
