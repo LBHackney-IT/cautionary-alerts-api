fbbad3a084210e3c6d2986a963e4abc1d8cb7210
humulla <43747248+humulla@users.noreply.github.com>
2023-01-04 12:27:42 +0000

    removed condition

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -85,16 +85,6 @@ data "aws_iam_policy_document" "sns_topic_policy" {
         "sns:Publish"
       ]
 
-      condition {
-        test     = "StringEquals"
-        variable = "AWS:SourceOwner"
-
-        values = [
-          data.aws_caller_identity.current.account_id
-        ]
-
-      }
-
       effect = "Allow"
 
       principals {
