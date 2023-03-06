c6eb1a589774673b95d527c37bfc2625a3e3173b
humulla <43747248+humulla@users.noreply.github.com>
2023-03-06 16:33:52 +0000

    added alertId to schema

--- database/schema.sql
+++ database/schema.sql
@@ -637,5 +637,6 @@ create table dbo."PropertyAlertNew"(
 	"mmh_id" varchar(36),
 	"outcome" varchar(100),
 	"assure_reference" varchar(12),
-	"is_active" Boolean NOT NULL
+	"is_active" Boolean NOT NULL, 
+	"alert_Id" varchar(36)
 );
